d5589bbb968b7d7edde56fa1a2ee4a22f3c30a48
Andrew <skkqw@yandex.ru>
2024-03-25 21:55:34 +0300

    Добавление окна выбора уровня

--- game_data.py
+++ game_data.py
@@ -9,3 +9,49 @@ level_0 = {
     'player': 'levels/0/level_0_player.csv',
     'grass': 'levels/0/level_0_grass.csv',
 }
+
+
+level_menu_0 = {
+    'node_pos': (110, 400),
+    'content': 'this is level 0',
+    'unlock': 1
+}
+
+level_menu_1 = {
+    'node_pos': (300, 220),
+    'content': 'this is level 0',
+    'unlock': 2
+}
+
+level_menu_2 = {
+    'node_pos': (480, 610),
+    'content': 'this is level 0',
+    'unlock': 3
+}
+
+level_menu_3 = {
+    'node_pos': (610, 350),
+    'content': 'this is level 0',
+    'unlock': 4
+}
+
+level_menu_4 = {
+    'node_pos': (880, 210),
+    'content': 'this is level 0',
+    'unlock': 5
+}
+
+level_menu_5 = {
+    'node_pos': (1050, 400),
+    'content': 'this is level 0',
+    'unlock': 5
+}
+
+levels = {
+    0: level_menu_0,
+    1: level_menu_1,
+    2: level_menu_2,
+    3: level_menu_3,
+    4: level_menu_4,
+    5: level_menu_5,
+}
